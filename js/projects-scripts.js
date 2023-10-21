9aede0002e0e4c3499369b046945b89e9b750816
sharper <hi@sammyharper.com>
2023-10-20 19:03:27 -0500

    feat: implement a working filter process using json

--- js/projects-scripts.js
+++ js/projects-scripts.js
@@ -2,85 +2,185 @@ window.addEventListener("load", function(){
 
   console.log("projects-scripts.js loaded");
 
+  // variables
+
+  // where the sections will go
+  var itemsContainer = document.querySelector("[data-sections-container]");
 
 // ************************************* 
 // LOAD JSON DATA
   const jsonFileLocation = "/js/projects.json";
 
+  function getProjects() {
+    // returns promise
+    return fetch(jsonFileLocation).then(function(response){
+      return response.json();
+    });
+  }
 
+  initialBuild();
 
+  // on first build, build all projects
+  function initialBuild(){
+    getProjects().then(function(response){
+      // Access the value inside the `then`
+      let projects = response.projectItems;
+    
+      clearProjects();
+      buildProjects(projects);
+      showPhotoSection();
+    })
+  }
 
+  // get filter triggers
+  var portfolioFilterButtons = document.querySelectorAll("[data-filter-button]");
 
+  if(portfolioFilterButtons.length > 0){
+    for(x = 0; x < portfolioFilterButtons.length; x++){
+      portfolioFilterButtons[x].addEventListener('click', getFilterBy);
+    }
+  }
 
+  function getFilterBy(){
+    var filterByVal = this.getAttribute("data-filter");
 
+    setFilter(filterByVal);
+  }
 
+  function setFilter(newFilter){
 
-// ************************************* 
-// DISPLAY THE NECESSARY SECTIONS
-  // created project section
-  var createdSection;
-
-  // where the sections will go
-  var itemsContainer = document.querySelector("[data-sections-container]");
-
-  // item template
-  var itemTemplate = document.getElementById("section-template");
-  var itemTemplateElem = itemTemplate.content.querySelector("[data-project-section]");
-
-  // get JSON data, build items
-  fetch(jsonFileLocation)
-    .then(response => response.json())
-      .then(data => {
-
-        for(let x = 0; x < data.projectItems.length; x++){
-
-          createdSection = document.importNode(itemTemplateElem, true);
-
-          // set ID
-          createdSection.id = data.projectItems[x].projectID + "-section";
-
-          // set innerHTML of title
-          createdSection.querySelector("[data-project-title]").innerHTML = data.projectItems[x].projectTitle;
-          createdSection.querySelector("[data-project-title]").dataHighlightText = data.projectItems[x].projectTitle;
-
-          // set caption
-          createdSection.querySelector("[data-project-caption]").innerHTML = data.projectItems[x].projectCaption;
-
-          //set img
-          createdSection.querySelector("[data-project-thumbnail]").src = data.projectItems[x].projectThumbnail;
-          
-
-          // set links (if one exists)
-          if(data.projectItems[x].projectLinkTitle !== ""){
-            // set img link
-            createdSection.querySelector("[data-project-thumbnail-link]").href = data.projectItems[x].projectLinkHref;
-          
-            // set text link
-            addlLink = createdSection.querySelector("[data-project-link]");
-            addlLink.innerHTML = data.projectItems[x].projectLinkTitle;
-
-            // fill in href
-            addlLink.href = data.projectItems[x].projectLinkHref;
-          }
-
-          // project tags
-          var arr = data.projectItems[x].projectTags;
-
-          // set text
-          createdSection.querySelector("[data-project-tags-container]").innerHTML = arr.join(", ");
-
-          // set filters
-          // separate multi-word filters with dashes instead of spaces
-          for(let y = 0; y < arr.length; y++){
-            arr[y] = arr[y].replace(/\s+/g, '-');
-          }
-          
-          createdSection.setAttribute('data-filter', arr.join(" "));
-
-          // append to container
-          itemsContainer.appendChild(createdSection);
-        }
+    if(newFilter == "all"){
+      getProjects().then(function(response){
+        // Access the value inside the `then`
+        let projects = response.projectItems;
+      
+        clearProjects();
+        buildProjects(projects);
+        showPhotoSection();
       })
+    }else if(newFilter == "photography"){
+      console.log("photography time!");
+
+      getProjects().then(function(response){
+        clearProjects();
+        showPhotoSection();
+      })
+    }else{
+      getProjects().then(function(response){
+        // Access the value inside the `then`
+        let projects = response.projectItems;
+        let filteredProjects = [];
+  
+        // for each project
+        for(let x = 0; x < projects.length; x++){
+  
+          // counter for if tag exists on this element
+          let tagExist = 0;
+  
+          // get this project's tags
+          var tagsArray = projects[x].projectTags;
+          // check all of their tags
+          for(let y = 0; y < tagsArray.length; y++){
+            // check projects for this in their tags
+            if(tagsArray[y] == newFilter){
+              // add to tagExist counter
+              tagExist++;
+            }
+          }
+  
+          // add the array item to the filteredProjects Array
+          if(tagExist > 0){
+            console.log("tag exists!");
+            filteredProjects.push(projects[x]);
+          }
+        }
+  
+        clearProjects();
+        buildProjects(filteredProjects);
+      })
+    }
+    
+    // Scroll to a certain element
+    document.querySelector("#portfolio-sections-container").scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
+
+  function clearProjects(){
+    console.log("clearing projects...")
+    itemsContainer.innerHTML = "";
+    itemsContainer.classList.remove("show-photo");
+  }
+
+
+  function buildProjects(projectsArray){
+    console.log("building projects...");
+
+    // created project section
+    var createdSection;
+
+    // item template
+    var itemTemplate = document.getElementById("section-template");
+    var itemTemplateElem = itemTemplate.content.querySelector("[data-project-section]");
+
+
+    for(let x = 0; x < projectsArray.length; x++){
+
+      createdSection = document.importNode(itemTemplateElem, true);
+
+      // set ID
+      createdSection.id = projectsArray[x].projectID + "-section";
+
+      // set innerHTML of title
+      createdSection.querySelector("[data-project-title]").innerHTML = projectsArray[x].projectTitle;
+      createdSection.querySelector("[data-project-title]").dataHighlightText = projectsArray[x].projectTitle;
+
+      // set caption
+      createdSection.querySelector("[data-project-caption]").innerHTML = projectsArray[x].projectCaption;
+
+      //set img
+      createdSection.querySelector("[data-project-thumbnail]").src = projectsArray[x].projectThumbnail;
+      
+      // set links (if one exists)
+      if(projectsArray[x].projectLinkTitle !== ""){
+        // set img link
+        createdSection.querySelector("[data-project-thumbnail-link]").href = projectsArray[x].projectLinkHref;
+      
+        // set text link
+        var addlLink = createdSection.querySelector("[data-project-link]");
+        addlLink.innerHTML = projectsArray[x].projectLinkTitle;
+
+        // fill in href
+        addlLink.href = projectsArray[x].projectLinkHref;
+      }
+
+      // project tags
+      var arr = projectsArray[x].projectTags;
+
+      // set text
+      var tagText = arr.join(", ");
+      tagText = tagText.replaceAll("-", " ");
+      createdSection.querySelector("[data-project-tags-container]").innerHTML = tagText;
+      
+      // set filter values
+      createdSection.setAttribute('data-filter', arr.join(" "));
+
+      // append to container
+      itemsContainer.appendChild(createdSection);
+    }
+  }
+
+  function showPhotoSection(){
+    itemsContainer.classList.add("show-photo");
+  }
+  
+  // get JSON data, build items
+  // fetch(jsonFileLocation)
+  //   .then(response => response.json())
+  //     .then(data => {
+            // stuff
+  //     })
 
   ///////////////
   
